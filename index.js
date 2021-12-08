88152890df3ca3e396d27f16cf583fbe34b1663e
tanyasinha2003 <64015556+tanyasinha2003@users.noreply.github.com>
2021-12-08 22:26:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+
+
+
+$("button.d").click(function(){
+  $("h3.work1").text("5 hrs");
+  $("h6.work2").text("Last week- 7hrs");
+  $("h3.play1").text("1 hrs");
+  $("h6.play2").text("Last week- 2hrs");
+  $("h3.study1").text("0 hrs");
+  $("h6.study2").text("Last week- 1hrs");
+  $("h3.exercise1").text("1 hrs");
+  $("h6.exercise2").text("Last week- 1hrs");
+  $("h3.social1").text("1 hrs");
+  $("h6.social2").text("Last week- 3hrs");
+  $("h3.self1").text("0 hrs");
+  $("h6.self2").text("Last week- 1hrs");
+})
+
+$("button.w").click(function(){
+  $("h3.work1").text("32 hrs");
+  $("h6.work2").text("Last week- 36hrs");
+  $("h3.play1").text("10 hrs");
+  $("h6.play2").text("Last week- 8hrs");
+  $("h3.study1").text("4 hrs");
+  $("h6.study2").text("Last week- 7hrs");
+  $("h3.exercise1").text("4 hrs");
+  $("h6.exercise2").text("Last week- 5hrs");
+  $("h3.social1").text("5 hrs");
+  $("h6.social2").text("Last week- 10hrs");
+  $("h3.self1").text("2 hrs");
+  $("h6.self2").text("Last week- 2hrs");
+})
+
+
+
+$("button.m").click(function(){
+  $("h3.work1").text("103 hrs");
+  $("h6.work2").text("Last week- 128hrs");
+  $("h3.play1").text("23 hrs");
+  $("h6.play2").text("Last week- 29hrs");
+  $("h3.study1").text("13 hrs");
+  $("h6.study2").text("Last week- 19hrs");
+  $("h3.exercise1").text("11 hrs");
+  $("h6.exercise2").text("Last week- 18hrs");
+  $("h3.social1").text("21 hrs");
+  $("h6.social2").text("Last week- 23hrs");
+  $("h3.self1").text("7 hrs");
+  $("h6.self2").text("Last week- 11hrs");
+})
